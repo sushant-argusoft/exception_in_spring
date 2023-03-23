b86a202aedba5e8692830dc2701d23398028d2c7
Sushant Sharma <ssharma@argusoft.com>
2023-03-23 10:17:37 +0530

    additional

--- src/main/java/com/sushant/restful/rest/StudentRestController.java
+++ src/main/java/com/sushant/restful/rest/StudentRestController.java
@@ -2,9 +2,10 @@ package com.sushant.restful.rest;
 
 import com.sushant.restful.entity.Student;
 import jakarta.annotation.PostConstruct;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -40,17 +41,6 @@ public class StudentRestController {
 
     // Add an exception handler using @ExceptionHandler
 
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc){
-       //create a StudentErrorResponse
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exc.getMessage());
-        error.setTimeStamp(System.currentTimeMillis());
 
-        //return ResponseEntity
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-
-    }
 
 }
---
+++ src/main/java/com/sushant/restful/rest/StudentRestExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.sushant.restful.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class StudentRestExceptionHandler {
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc){
+        //create a StudentErrorResponse
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        //return ResponseEntity
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+
+    }
+}
